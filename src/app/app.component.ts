9b6e84c5a74c60c50495cfc5e729dc238cda092e
Daniel Ivan Guerrero Ponce <danielivanguerreroponce@gamil.com>
2025-02-17 23:37:02 -0600

    Acordeones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InicioSesionComponent } from "./sesion/inicio-sesion/inicio-sesion.component";
+import { AlumnoVistaComponent } from "./alumno/alumno-vista/alumno-vista.component";
+import { TareasComponent } from "./alumno/tareas/tareas.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InicioSesionComponent],
+  imports: [RouterOutlet, InicioSesionComponent, AlumnoVistaComponent, TareasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
