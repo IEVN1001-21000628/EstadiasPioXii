86977292dc84e86a5b44ecffce054fe3676e825d
Daniel Ivan Guerrero Ponce <danielivanguerreroponce@gamil.com>
2025-02-18 20:18:39 -0600

    perfiles de profes listo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { InicioSesionComponent } from "./sesion/inicio-sesion/inicio-sesion.component";
 import { AlumnoVistaComponent } from "./alumno/alumno-vista/alumno-vista.component";
 import { TareasComponent } from "./alumno/tareas/tareas.component";
+import { CalificacionProfesoresComponent } from "./profesores/calificacion-profesores/calificacion-profesores.component";
+import { TareaProfesoresComponent } from "./profesores/tarea-profesores/tarea-profesores.component";
+import { ListaCalificacionesComponent } from "./profesores/lista-calificaciones/lista-calificaciones.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InicioSesionComponent, AlumnoVistaComponent, TareasComponent],
+  imports: [RouterOutlet, InicioSesionComponent, AlumnoVistaComponent, TareasComponent, CalificacionProfesoresComponent, TareaProfesoresComponent, ListaCalificacionesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/profesores/lista-calificaciones/lista-calificaciones.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-calificaciones',
+  standalone: true,
+  imports: [],
+  templateUrl: './lista-calificaciones.component.html',
+  styleUrl: './lista-calificaciones.component.css'
+})
+export class ListaCalificacionesComponent {
+
+}
